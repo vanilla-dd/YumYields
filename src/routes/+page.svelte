ffdba81d51ecaacf09588fee2fe46a7598b26974
vanilla-dd <freeidlll6@gmail.com>
2024-04-14 11:58:00 +0530

    feat: github auth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { signIn, signOut } from '@auth/sveltekit/client';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={() => signIn('github')}>signin</button>
+<button on:click={() => signOut()}>signout</button>
+{JSON.stringify($page.data.session?.user)}
